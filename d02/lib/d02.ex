f3e34b25dd9b7cb17df0d9db0ab7227a62d86330
Lai Weng Han <jlwhoo7@gmail.com>
2017-12-08 08:15:55 +0800

    Done D2

--- d02/lib/d02.ex
+++ d02/lib/d02.ex
@@ -2,17 +2,50 @@ defmodule D02 do
   @moduledoc """
   Documentation for D02.
   """
+  def index do
+    fileRead("input.txt")
+    |> processFile
+    |> main
+  end
 
+  
   @doc """
-  Hello world.
 
   ## Examples
-
-      iex> D02.hello
-      :world
-
+      iex> b = "1\\t2\\t3\\n4\\t5\\t6"
+      iex> D02.processFile(b)
+      [[1, 2, 3], [4, 5, 6]]
   """
-  def hello do
-    :world
+  def processFile(input) do
+    input
+    |> String.split("\n")
+    |> Enum.map(fn x -> String.split(x,"\t") end)
+    |> Enum.map(fn x -> Enum.map(x, fn y -> String.to_integer(y) end) end)
+  end
+
+
+  @doc """
+
+  ## Examples
+      iex> a = [[1,2,3], [4,5,6]]
+      iex> D02.main(a)
+      4
+  """
+  def main(input) do
+    res = input
+    |> Enum.map(fn x -> findDifference(x) end)
+    |> List.flatten
+    |> Enum.reduce(fn (x, y) -> x + y end)
+
+    IO.inspect res
+  end
+
+  def fileRead(filename) do
+    {:ok, input} = File.read(filename)
+    input
+  end
+
+  def findDifference(input) do
+    [Enum.max(input) - Enum.min(input)]
   end
 end
--- d02/test/d02_test.exs
+++ d02/test/d02_test.exs
@@ -1,8 +1,4 @@
 defmodule D02Test do
   use ExUnit.Case
   doctest D02
-
-  test "greets the world" do
-    assert D02.hello() == :world
-  end
 end
